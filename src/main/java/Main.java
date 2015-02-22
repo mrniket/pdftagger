201c3dd44cccc5ae556d6123cd6b2f456e5cadec
Niket Shah <masterniket@gmail.com>
2015-02-22 13:20:50 +0000

    add base main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+/**
+ * Created by Niket on 23/12/14.
+ */
+public class Main {
+    public static final String RESOURCES_DIR = "src/main/resources/";
+
+    /**
+     * Main method.
+     * @param    args    no arguments needed
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+
+    }
+}
